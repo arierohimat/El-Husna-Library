cbac7d8ef0493168a4b34fc26cbddcc1b5099d2a
arierohimat <arierohimat21@gmail.com>
2026-01-26 04:57:23 +0700

    ngulang

--- src/app/dashboard/member/page.tsx
+++ src/app/dashboard/member/page.tsx
@@ -1,5 +1,3 @@
-export const dynamic = "force-dynamic";
-
 import { redirect } from "next/navigation";
 import { DashboardLayout } from "@/components/layout/dashboard-layout";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";

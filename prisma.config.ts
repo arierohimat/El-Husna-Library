c385a472b24b896d4abb2699179f4dcc81b84d6c
arierohimat <arierohimat21@gmail.com>
2026-01-25 21:28:07 +0700

    fix: downgrade to prisma v6 stable and remove prisma.config.ts

--- prisma.config.ts
+++
@@ -1,9 +0,0 @@
-// @ts-ignore
-import { defineConfig } from "prisma/config";
-
-export default defineConfig({
-  migrate: {
-    datasource: "db",
-    datasourceUrl: process.env.DATABASE_URL!,
-  },
-});
